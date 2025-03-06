e3be2f678d6cf15f08512e9a03f948a68e09e20c
Filip Biterski <filip.biterski@filibit.com>
2025-03-06 12:41:09 +0100

    test: define error messages

---
+++ src/errors.js
@@ -0,0 +1,6 @@
+export const MISSING_START_CHARACTER = "Missing start character";
+export const MISSING_END_CHARACTER = "Missing end character";
+export const MULTIPLE_START_CHARACTERS = "Multiple start characters";
+export const FORK_DETECTED = "Path has a fork";
+export const BROKEN_PATH = "Path is broken";
+export const FAKE_TURN_REACHED = "Fake turn reached";
