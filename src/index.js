06c8914ea529fadb2603b7f3c12ffb7988330f15
Filip Biterski <filip.biterski@filibit.com>
2025-03-14 12:50:56 +0100

    refactor: update default playground config

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import { makeCharacterMap } from "./character-map/character-map.js";
 import { makeMatrix } from "./lib/make-matrix.js";
 import { travel } from "./travel.js";
 
-const STRING_MAP = `  @---A---+
-          |
-  x-B-+   C
-      |   |
-      +---+`;
+const STRING_MAP = `  @
+  | +-C--+
+  A |    |
+  +---B--+
+    |      x
+    |      |
+    +---D--+`;
 
 const matrix = makeMatrix(STRING_MAP);
 const charMap = makeCharacterMap(matrix);
