315664caef63ae32a71446a1a237ab7cf7cda6fe
Filip Biterski <filip.biterski@filibit.com>
2025-03-14 11:21:41 +0100

    feat: crossroad-repository

--- src/crossroad-repository.js
+++ src/crossroad-repository.js
@@ -1,15 +1,63 @@
+import { OPPOSITE_DIRECTION } from "./position/direction.js";
+import { addVectors, VECTOR } from "./position/vector.js";
+
 export class CrossroadRepository {
+    /**
+     * @private
+     * @type {Map<string, Set<string>>}
+     */
     static lockedGates = new Map();
 
-    static isGateLocked() {}
+    /**
+     * @param {import("./position/type.js").Vector} position
+     * @param {import("./position/type.js").Direction} direction
+     * @returns {boolean}
+     */
+    static isGateLocked(position, direction) {
+        return !!(
+            this.lockedGates.get(position.join(","))?.has(direction) ||
+            this.lockedGates
+                .get(addVectors(position, VECTOR[direction]).join(","))
+                ?.has(OPPOSITE_DIRECTION[direction])
+        );
+    }
 
-    static lockGate() {}
+    /**
+     * @param {import("./position/type.js").Vector} position
+     * @param {import("./position/type.js").Direction} direction
+     */
+    static lockGate(position, direction) {
+        const paths = this.lockedGates.get(position.join(",")) || new Set();
 
-    static unlockGatesAt() {}
+        this.lockedGates.set(
+            position.join(","),
+            new Set([...paths, direction]),
+        );
+    }
 
-    static unlockGate() {}
+    /**
+     * @param {import("./position/type.js").Vector} position
+     */
+    static unlockGatesAt(position) {
+        CrossroadRepository.lockedGates.delete(position.join(","));
+    }
 
-    static isVisited() {}
+    /**
+     * @param {import("./position/type.js").Vector} position
+     * @param {import("./position/type.js").Direction} direction
+     */
+    static unlockGate(position, direction) {
+        this.lockedGates.get(position.join(","))?.delete(direction);
+    }
 
-    static clear() {}
+    /**
+     * @param {import("./position/type.js").Vector} position
+     */
+    static isVisited(position) {
+        return this.lockedGates.has(position.join(","));
+    }
+
+    static clear() {
+        this.lockedGates.clear();
+    }
 }
