9d26f2c4cb71d99b0c32428051ed1492390b2403
Filip Biterski <filip.biterski@filibit.com>
2025-03-14 11:18:08 +0100

    feat: crossroad-repository

---
+++ src/crossroad-repository.js
@@ -0,0 +1,15 @@
+export class CrossroadRepository {
+    static lockedGates = new Map();
+
+    static isGateLocked() {}
+
+    static lockGate() {}
+
+    static unlockGatesAt() {}
+
+    static unlockGate() {}
+
+    static isVisited() {}
+
+    static clear() {}
+}
