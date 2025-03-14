0d88d0eed7e990f0d8e1eb06ef05ea028b57eff4
Filip Biterski <filip.biterski@filibit.com>
2025-03-14 11:57:30 +0100

    feat: complete travel()

--- src/travel.js
+++ src/travel.js
@@ -1,1 +1,23 @@
-export function travel() {}
+import { handleStart } from "./character-handlers/handle-start.js";
+import { START_CHARACTER } from "./characters.js";
+import { getCharPosition } from "./lib/get-char-position.js";
+import { makeCharacterMap } from "./character-map/character-map.js";
+
+/**
+ *
+ * @param {string[][]} inputMap
+ */
+export function travel(inputMap) {
+    console.log("=== START ===");
+    const charMap = makeCharacterMap(inputMap);
+
+    const startPosition = getCharPosition(charMap, START_CHARACTER);
+
+    console.log(`The map:\n\n${charMap.map((r) => r.join("")).join("\n")}`);
+
+    const result = handleStart(charMap, startPosition);
+
+    console.log(`Letters: ${result[0]}\nCharacter trail: ${result[1]}`);
+    console.log("===  END  ===");
+    return result;
+}
