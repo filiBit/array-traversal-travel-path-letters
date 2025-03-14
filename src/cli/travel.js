fa9c314c805c4ecb8c74de9ccc668c09b60a5ce4
Filip Biterski <filip.biterski@filibit.com>
2025-03-14 12:51:46 +0100

    refactor: cli/travel output

--- src/cli/travel.js
+++ src/cli/travel.js
@@ -2,4 +2,4 @@ import { makeCharacterMap } from "../character-map/character-map.js";
 import { makeMatrix } from "../lib/make-matrix.js";
 import { travel } from "../travel.js";
 
-console.log(travel(makeCharacterMap(makeMatrix(process.argv[2]))));
+travel(makeCharacterMap(makeMatrix(process.argv[2])));
